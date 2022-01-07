c553eaa8d0e35de2539fa225e0c41ef6dd550834
TheDoctor2018 <carlosivasquez2018@gmail.com>
2022-01-07 09:00:46 -0600

    Added changeup and Pattern values to create change the patter after x amount of times. Simple but does changes things up.

--- Challenges/ZigZag/ZigZag.py
+++ Challenges/ZigZag/ZigZag.py
@@ -4,18 +4,33 @@
 
 import time,sys
 indent = 0 #How many space to indent.
+changeup = 0 # how many times the patterns reaches 20 before its changed.
 indentIncreasing = True #Whether the indentation is increasing or not. 
+Pattern = 'L'
+
 
 try:
     while True: #The main program loop.
         print(' '*indent, end='')
-        print('LLLLLLLL')
+
+        print(Pattern,Pattern,Pattern,Pattern,Pattern,Pattern,Pattern,Pattern)
+
         time.sleep(0.1)#pause for 1/10 of a second
 
         if indentIncreasing:
             #Increase the number of spaces:
             indent = indent +1
             if indent == 20:
+
+                changeup = changeup +1
+                #change the pattern
+                if changeup == 2:
+                    Pattern = 'M'
+                if changeup == 4:
+                    Pattern = 'O'
+                if changeup == 6:
+                    Pattern = 'L'
+                    changeup = 0 #resets
                 #change direction
                 indent = False
                 
@@ -27,5 +42,3 @@ try:
                 indentIncreasing = True
 except KeyboardInterrupt:
     sys.exit()
-
-
